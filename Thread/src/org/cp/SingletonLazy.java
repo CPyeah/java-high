ed35c32dbab7bd49c87de393741be698f4c31f79
“CPyeah <chengpengyeah@126.com>
2019-07-25 16:30:05 +0800

    单例模式，懒汉式 线程安全改写；高效if判断 测试类添加

--- Thread/src/org/cp/SingletonLazy.java
+++ Thread/src/org/cp/SingletonLazy.java
@@ -11,10 +11,39 @@ public class SingletonLazy {
     private static SingletonLazy singletonLazy;
 
     public static SingletonLazy getInstance() {
-        if (singletonLazy==null) {
-            singletonLazy = new SingletonLazy();
+        if (singletonLazy==null) {//高效判断，不用每次都进来锁里面，
+            synchronized (SingletonLazy.class) {
+                if (singletonLazy==null) {
+                    try {
+                        Thread.sleep(100);//延迟看效果
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    singletonLazy = new SingletonLazy();
+
+                }
+            }
         }
+        System.out.println(singletonLazy);//引用地址一样表示线程安全
         return singletonLazy;
     }
 
+}
+class SingletonLazyTest{
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(){
+            @Override
+            public void run() {
+                SingletonLazy.getInstance();
+            }
+        };
+        Thread thread2 = new Thread(){
+            @Override
+            public void run() {
+                SingletonLazy.getInstance();
+            }
+        };
+        thread1.start();
+        thread2.start();
+    }
 }
