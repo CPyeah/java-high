6eedb84566e4d61ef5286f7c89bcd2bf71524631
“CPyeah <chengpengyeah@126.com>
2019-07-26 14:11:50 +0800

    通过Lock实现线程安全，一个账户，两个客户操作；每次存一千，每人存三次，存完打印余额

---
+++ Thread/src/org/cp/BankSimpleTestByLock.java
@@ -0,0 +1,75 @@
+package org.cp;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * create by CP on 2019/7/26 0026.
+ */
+public class BankSimpleTestByLock {
+    public static void main(String[] args) {
+        AccountB account = new AccountB();
+        CustomerB customer1 = new CustomerB(account);
+        CustomerB customer2 = new CustomerB(account);
+        customer1.setName("客户1");
+        customer2.setName("客户2");
+        customer1.start();
+        customer2.start();
+    }
+}
+
+class AccountB {
+    private Double balance=0.0;//余额
+
+    private Lock lock = new ReentrantLock();//锁
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Lock getLock() {
+        return lock;
+    }
+
+    public void setLock(Lock lock) {
+        this.lock = lock;
+    }
+}
+
+class CustomerB extends Thread{
+
+    AccountB account;
+
+    public CustomerB(AccountB account) {
+        this.account = account;
+    }
+
+    public void deposit(Double amount) {
+        try {
+            account.getLock().lock();
+            Double balance = account.getBalance();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Double newBalance = balance+amount;
+            account.setBalance(newBalance);
+            System.out.println(Thread.currentThread().getName()+"存入 "+amount
+            +" 元, 现在余额为： "+newBalance +" 元");
+        } finally {
+            account.getLock().unlock();
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            deposit(1000.0);
+        }
+    }
+}
