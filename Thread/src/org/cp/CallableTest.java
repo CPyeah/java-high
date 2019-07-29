4a1371e3596292104cbc69714726f4099875be8d
“CPyeah <chengpengyeah@126.com>
2019-07-29 14:47:43 +0800

    callable初体验
    by cp profeta

---
+++ Thread/src/org/cp/CallableTest.java
@@ -0,0 +1,33 @@
+package org.cp;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Callable初体验
+ * create by CP on 2019/7/29 0029.
+ */
+public class CallableTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CC cc = new CC();
+        FutureTask task = new FutureTask(cc);//封装Callable实现类
+        Thread thread = new Thread(task);
+        thread.start();
+        Object o = task.get();//阻塞式获取
+        System.out.println(o);
+    }
+}
+
+class CC implements Callable {
+
+    @Override
+    public Object call() throws Exception {
+        Integer sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum+=i;
+        }
+        Thread.sleep(5000);
+        return sum;
+    }
+}
