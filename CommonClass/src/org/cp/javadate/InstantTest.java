a3d69137733e4df5db341b0435882c9954cd2c67
“CPyeah <chengpengyeah@126.com>
2019-08-09 14:14:41 +0800

    Instant 类
    by cp profeta

--- CommonClass/src/org/cp/javadate/InstantTest.java
+++ CommonClass/src/org/cp/javadate/InstantTest.java
@@ -19,4 +19,13 @@ public class InstantTest {
         OffsetDateTime offsetDateTime = now.atOffset(ZoneOffset.ofHours(8));// 偏移8小时
         System.out.println(offsetDateTime);// 2019-08-09T14:07:44.855+08:00
     }
+    
+    @Test
+    public void test2() {
+        //1970-1-1日开始往后数这么多毫秒数 得到的Instant
+        Instant instant = Instant.ofEpochMilli(2341234123412L);
+        System.out.println(instant);//2044-03-10T14:48:43.412Z
+        long l = instant.toEpochMilli();//转成时间戳
+        System.out.println(l);//2341234123412
+    }
 }
