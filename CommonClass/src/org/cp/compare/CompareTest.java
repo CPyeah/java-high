faaa66fb878d5007a501dc615eef28cd022b8482
“CPyeah <chengpengyeah@126.com>
2019-08-13 15:31:34 +0800

    java类 比较
    by cp profeta

--- CommonClass/src/org/cp/compare/CompareTest.java
+++ CommonClass/src/org/cp/compare/CompareTest.java
@@ -12,6 +12,11 @@ package org.cp.compare;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * 〈一句话功能简述〉<br> 
  * 〈java类 比较〉
@@ -29,7 +34,16 @@ public class CompareTest {
      */
     @Test
     public void stringArrayCompareTest() {
-
+        String[] strings = new String[7];
+        strings[0] = "BB";
+        strings[1] = "CC";
+        strings[2] = "FF";
+        strings[3] = "AA";
+        strings[4] = "DD";
+        strings[5] = "CCC";
+        strings[6] = "EE";
+        Arrays.sort(strings);
+        System.out.println(Arrays.toString(strings));//[AA, BB, CC, CCC, DD, EE, FF]
     }
 
     /**
@@ -37,7 +51,17 @@ public class CompareTest {
      */
     @Test
     public void mouseCompareTest() {
-
+        ArrayList<Mouse> mice = new ArrayList<>();
+        mice.add(new Mouse("dell", 60.0));
+        mice.add(new Mouse("hp", 77.0));
+        mice.add(new Mouse("mi", 40.0));
+        Collections.sort(mice);
+        for (Mouse m : mice) {
+            System.out.println(m);
+        }
+        //Mouse{name='mi', price=40.0}
+        //Mouse{name='dell', price=60.0}
+        //Mouse{name='hp', price=77.0}
     }
 
     /**
@@ -51,7 +75,21 @@ public class CompareTest {
      */
     @Test
     public void comparatorStringArrayTest() {
-
+        String[] strings = new String[7];
+        strings[0] = "BB";
+        strings[1] = "CC";
+        strings[2] = "FF";
+        strings[3] = "AA";
+        strings[4] = "DD";
+        strings[5] = "CCC";
+        strings[6] = "EE";
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        System.out.println(Arrays.toString(strings));//[FF, EE, DD, CCC, CC, BB, AA]
     }
 
     /**
@@ -59,7 +97,23 @@ public class CompareTest {
      */
     @Test
     public void comparatorMouseTest() {
-
+        ArrayList<Mouse> mice = new ArrayList<>();
+        mice.add(new Mouse("dell", 60.0));
+        mice.add(new Mouse("hp", 77.0));
+        mice.add(new Mouse("mi", 40.0));
+        //按名称大小倒序
+        Collections.sort(mice, new Comparator<Mouse>() {
+            @Override
+            public int compare(Mouse o1, Mouse o2) {
+                return -o1.getName().compareTo(o2.getName());
+            }
+        });
+        for (Mouse m : mice) {
+            System.out.println(m);
+        }
+        //Mouse{name='mi', price=40.0}
+        //Mouse{name='hp', price=77.0}
+        //Mouse{name='dell', price=60.0}
     }
 
 
--- CommonClass/src/org/cp/compare/Mouse.java
+++ CommonClass/src/org/cp/compare/Mouse.java
@@ -19,10 +19,44 @@ package org.cp.compare;
  * @create 2019/8/12
  * @since 1.0.0
  */
-public class Mouse {
+public class Mouse implements Comparable{
 
     private String name;
 
     private Double price;
 
+    public Mouse(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Mouse{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Mouse m = (Mouse) o;
+        return this.price.compareTo(m.price);
+    }
 }
