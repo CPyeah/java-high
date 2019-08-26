55afbbef262e5d4d2bc90d4294592a9ba6f81252
“CPyeah <chengpengyeah@126.com>
2019-08-26 16:28:13 +0800

    Collections 工具类 测试
    by cp profeta

--- CommonClass/src/org/cp/collection/CollectionsTest.java
+++ CommonClass/src/org/cp/collection/CollectionsTest.java
@@ -2,10 +2,7 @@ package org.cp.collection;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Collections 工具类 测试
@@ -20,6 +17,7 @@ public class CollectionsTest {
         list.add("B");
         list.add("F");
         list.add("E");
+//        list.add("A");
     }
 
     /**
@@ -60,9 +58,51 @@ public class CollectionsTest {
         printList();//[F, E, C, B, A]
     }
 
+    /**
+     * swap(List，int， int)：将指定 list 集合中的 i 处元素和 j 处元素进行交换
+     */
+    @Test
+    public void swapTest() {
+        printList();//[A, C, B, F, E]
+        Collections.swap(list, 2,3);
+        printList();//[A, C, F, B, E]
+    }
+
+    /**
+     * int frequency(Collection，Object)：返回指定集合中指定元素的出现次数
+     */
+    @Test
+    public void frequencyTest() {
+        printList();//[A, C, B, F, E, A]
+        int count = Collections.frequency(list, "A");
+        System.out.println(count);//2
+    }
+
+    /**
+     * void copy(List dest,List src)：将src中的内容复制到dest中
+     */
+    @Test
+    public void copyTest() {
+//        List newList = new ArrayList();//直接new行不通, Source does not fit in dest
+        List newList = Arrays.asList(new Object[list.size()]);//需要初始化相同大小的容器
+        System.out.println(newList);//[null, null, null, null, null]
+        Collections.copy(newList, list);
+        System.out.println(newList);//[A, C, B, F, E]
+    }
+
+    /**
+     * boolean replaceAll(List list，Object oldVal，Object newVal)：使用新值替换 List 对象的所旧值
+     */
+    @Test
+    public void replaceTest() {
+        printList();//[A, C, B, F, E]
+        Collections.replaceAll(list, "C", "Z");
+        printList();//[A, Z, B, F, E]
+    }
+
     //打印列表工具方法
     private void printList() {
-        System.out.println(list);
+        System.out.println("//"+list);
         System.out.println("***************************");
     }
 
