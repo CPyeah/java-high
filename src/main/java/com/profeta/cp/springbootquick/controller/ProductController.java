44c1d5f0faa1f058802d95bd26342e7e92d6f48a
“CPyeah <chengpengyeah@126.com>
2019-08-29 10:19:54 +0800

    generics simple test
    by cp profeta

--- src/main/java/com/profeta/cp/springbootquick/controller/ProductController.java
+++ src/main/java/com/profeta/cp/springbootquick/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import static com.profeta.cp.springbootquick.dao.ProductsInit.productsList;
@@ -45,6 +46,41 @@ public class ProductController {
         newProduct.setId(id);
         boolean r = productsList.add(newProduct);
         result.put("result", r);
+        return result;
+    }
+
+    @PutMapping("/product/{id}")
+    public Map product(@RequestBody Product product, @PathVariable Integer id) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("code", 0000);
+        for (int i = 0; i < productsList.size(); i++) {
+            Product item = productsList.get(i);
+            if (item.getId().equals(id)) {
+                product.setId(id);
+                Product r = productsList.set(i, product);
+                result.put("result", r);
+                return result;
+            }
+        }
+        result.put("code", 404);
+        result.put("result", "this id(" + id + ") is not exist.");
+        return result;
+    }
+
+    @DeleteMapping("/product/{id}")
+    public Map delProduct(@PathVariable Integer id) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("code", 0000);
+        Iterator<Product> iterator = productsList.iterator();
+        while (iterator.hasNext()) {
+            Product item = iterator.next();
+            if (item.getId().equals(id)) {
+                iterator.remove();
+                return result;
+            }
+        }
+        result.put("code", 404);
+        result.put("result", "this id(" + id + ") is not exist.");
         return result;
     }
 
