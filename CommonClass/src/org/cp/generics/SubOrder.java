0439075bcbb3dcac29c390db11142447c03faf4d
“CPyeah <chengpengyeah@126.com>
2019-08-30 09:47:04 +0800

    泛型与static
    by cp profeta

--- CommonClass/src/org/cp/generics/SubOrder.java
+++ CommonClass/src/org/cp/generics/SubOrder.java
@@ -9,8 +9,18 @@ public class SubOrder<T> extends Order<T>{
     public SubOrder(int i, String subOrder, T v) {
     }
 
+    //正常带泛型的方法, 不能声明为静态的. 因为static先加载, 这时候泛型T还未确定
+//    public static T valuesOf1(T key) {
+//        return (T) new Object();
+//    }
+
     //这个泛型方法
     public <E>E valueOf(E key) {
+        return key;
+    }
+
+    //静态方法也可以
+    public static <K>K valuesOf(K key) {
         return key;
     }
 }
