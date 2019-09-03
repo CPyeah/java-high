f12eaca72598662b03f23cbfc840ab303701b865
“CPyeah <chengpengyeah@126.com>
2019-09-03 09:22:35 +0800

    File 类 测试
    by cp profeta

--- FileIOStream/src/org/cp/file/FileTest.java
+++ FileIOStream/src/org/cp/file/FileTest.java
@@ -86,5 +86,41 @@ public class FileTest {
     public void createTest() {
     
     }
+
+    /**
+     * 删除测试
+     */
+    @Test
+    public void deleteTest() {
+    
+    }
+
+    /**
+     * 1. 利用File构造器，new 一个文件目录file
+     *      1)在其中创建多个文件和目录
+     *      2)编写方法，实现删除file中指定文件的操作
+     */
+    @Test
+    public void practice1() {
+    
+    }
+
+    /**
+     * 2. 判断指定目录下是否有后缀名为.jpg的文件，如果有，就输出该文件名称
+     */
+    @Test
+    public void practice2() {
+
+    }
+
+    /**
+     * 3. 遍历指定目录所有文件名称，包括子文件目录中的文件。
+     *      拓展1：并计算指定目录占用空间的大小
+     *      拓展2：删除指定文件目录及其下的所有文件
+     */
+    @Test
+    public void practice3() {
+
+    }
     
 }
