8400aecb14fc445378070bf113eab6694de79111
“CPyeah <chengpengyeah@126.com>
2019-09-09 14:10:49 +0800

    获取文本文件中， 每一个字符出现的次数
    by cp profeta

--- FileIOStream/src/org/cp/io/Practice.java
+++ FileIOStream/src/org/cp/io/Practice.java
@@ -3,6 +3,7 @@ package org.cp.io;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * 关于流的一些练习
@@ -34,6 +35,47 @@ public class Practice {
             e.printStackTrace();
         } finally {
             NodeStream.closeStream(img, img_encrypted);
+        }
+    }
+
+    /**
+     * 获取文本文件中， 每一个字符出现的次数
+     */
+    @Test
+    public void charCounter() {
+        HashMap<Character, Integer> counterMap = new HashMap<>();
+        BufferedReader txtReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            txtReader = new BufferedReader(new FileReader("aa/text.txt"));
+            bufferedWriter = new BufferedWriter(new FileWriter("aa/txt_counter.txt"));
+            int data;
+            while ((data=txtReader.read()) != -1) {
+                char c = (char) data;
+                if (counterMap.containsKey(c)) {
+                    Integer count = counterMap.get(c);
+                    count++;
+                    counterMap.put(c, count);
+                } else {
+                    counterMap.put(c, 1);
+                }
+//                System.out.println(c);
+            }
+            Set<Map.Entry<Character, Integer>> entries = counterMap.entrySet();
+            ArrayList<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
+            Collections.sort(list, Comparator.comparing(Map.Entry::getValue));
+            for (Map.Entry<Character, Integer> item : list) {
+                Character key = item.getKey();
+                Integer value = item.getValue();
+                bufferedWriter.write(key + ": " + value);
+                bufferedWriter.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            NodeStream.closeStream(txtReader, bufferedWriter);
         }
     }
     
