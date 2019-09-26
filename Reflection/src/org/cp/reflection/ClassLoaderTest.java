7991b41d3b0b4ad6232912a0c6a545755b6346f8
“CPyeah <chengpengyeah@126.com>
2019-09-26 10:05:39 +0800

    ClassLoader test
    by cp profeta

--- Reflection/src/org/cp/reflection/ClassLoaderTest.java
+++ Reflection/src/org/cp/reflection/ClassLoaderTest.java
@@ -2,6 +2,7 @@ package org.cp.reflection;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -19,9 +20,26 @@ public class ClassLoaderTest {
         ClassLoader classLoader = this.getClass().getClassLoader();
         //以src目录为根目录
         InputStream stream = classLoader.getResourceAsStream("jdbc.properties");
+        FileInputStream fis = new FileInputStream("jdbc.properties");
         Properties properties = new Properties();
         properties.load(stream);
+//        properties.load(fis);// bundle 目录下的jdbc
         String name = properties.getProperty("name");
         System.out.println(name);
+    }
+
+    /**
+     * 三种类加载器
+     */
+    @Test
+    public void threeClassLoader() {
+        ClassLoader classLoader1 = Person.class.getClassLoader();
+        ClassLoader classLoader2 = classLoader1.getParent();
+        ClassLoader classLoader3 = classLoader2.getParent();
+        ClassLoader classLoader4 = String.class.getClassLoader();
+        System.out.println(classLoader1);//sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println(classLoader2);//sun.misc.Launcher$ExtClassLoader@452b3a41
+        System.out.println(classLoader3);// 核心加载器获取不到
+        System.out.println(classLoader4);// 核心加载器获取不到
     }
 }
